56dc52719a7ecd29076e84ce5ada508859e66258
jyashi <navtejdhot@gmail.com>
2022-10-07 16:33:32 +0900

    c

--- app/src/main/java/com/example/kotlinrickmortyapi/components/LazyListColumn.kt
+++ app/src/main/java/com/example/kotlinrickmortyapi/components/LazyListColumn.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -61,8 +62,8 @@ fun LazyListColumn(mainModel: MainViewModel, navController: NavController = reme
                     .align(BottomCenter)
                     .padding(vertical = 50.dp)
                     .background(
-                        color = MaterialTheme.colors.secondaryVariant.copy(alpha = 0.5f),
-//                        color = Color.DarkGray.copy(alpha = 0.5f),
+//                        color = MaterialTheme.colors.secondaryVariant.copy(alpha = 0.5f),
+                        color = Color.DarkGray.copy(alpha = 0.5f),
                         shape = RoundedCornerShape(50.dp)
                     )) {
                 if(mainModel.page.value != 1){
